fb829112bf488ad2040738a71c4b14141d1dd3c6
kirkwilson-snhu <kirkdevin17@gmail.com>
2023-10-01 23:03:10 -0400

    module5

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -67,4 +67,4 @@ process.on('SIGTERM', () => {
 connect();
 
 // bring in the Mongoose schema
-require('./models/travlr');
+require("./models/travlr");
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,8 @@
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
+
 var fs = require('fs');
 
 var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
@@ -9,4 +14,43 @@ const travel = (req, res) => {
    module.exports = {
     travel
    };
-   
+
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(requestOptions, (err, response, body) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        renderTravelList(req, res, body);
+    });
+};
+
+// internal method to render the travel list
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    res.render('travel',
+        {
+            title: pageTitle,
+            trips: responseBody,
+            message
+        }
+    );
+}
